f9fd4f10c4857fb6f5076da85b987d4a0cde13ed
milo <juancamilomendozabalanta.jcmb@gmail.com>
2020-02-19 18:32:50 -0500

    save changes

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -3,7 +3,7 @@ const config = {
   auth0_client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
   redirect_uri: `${window.location.origin}/home`,
   api_url:
-    process.env.REACT_APP_ENV === 'DEV'
+    process.env.REACT_APP_ENV !== 'PROD'
       ? process.env.REACT_APP_API_URL_DEV
       : process.env.REACT_APP_API_URL_PROD,
   movie_key: process.env.REACT_APP_MOVIE_KEY,
