98cf8354d7fb870814886774e6a5c0ec0fc21f48
milo <juancamilomendozabalanta.jcmb@gmail.com>
2020-02-20 10:36:11 -0500

    work in loading

---
+++ build/precache-manifest.9dfa3e6216f235811610480a9c495655.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88577856605cdeba9a14ab12cc8dcf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44971879191eb6444b15",
+    "url": "/static/css/main.e1b9a128.chunk.css"
+  },
+  {
+    "revision": "462532afe62f35f82447",
+    "url": "/static/js/2.f8a45261.chunk.js"
+  },
+  {
+    "revision": "bef5aea39084748a6a8e0c90917a84bc",
+    "url": "/static/js/2.f8a45261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44971879191eb6444b15",
+    "url": "/static/js/main.bf572948.chunk.js"
+  },
+  {
+    "revision": "10eaa634e3c115aead3f",
+    "url": "/static/js/runtime-main.51b7d066.js"
+  },
+  {
+    "revision": "2ac8e864c65dc569959f212f415fe334",
+    "url": "/static/media/login.2ac8e864.svg"
+  },
+  {
+    "revision": "2f6ed3a3de23a9f7c38d77c99d033d3e",
+    "url": "/static/media/logo.2f6ed3a3.svg"
+  },
+  {
+    "revision": "5ec9cd16a0d6304d4646fdad493ad46a",
+    "url": "/static/media/profile.5ec9cd16.svg"
+  }
+]);
